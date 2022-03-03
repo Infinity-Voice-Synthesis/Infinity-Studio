4ff9e0fe2d918817e8754d1f28ec168ba5abf5d2
無常 <3142324836@qq.com>
2022-03-03 19:20:06 +0800

    Change Encoding

--- resource.h
+++ resource.h
@@ -1,6 +1,6 @@
-//{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ Éú³ÉµÄ°üº¬ÎÄ¼ş¡£
-// ¹© resources.rc Ê¹ÓÃ
+ï»¿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ ç”Ÿæˆçš„åŒ…å«æ–‡ä»¶ã€‚
+// ä¾› resources.rc ä½¿ç”¨
 //
 #define SW_HIDE                         0
 #define HIDE_WINDOW                     0
